e0dfd2fff8aae939e92b7b15a82f6b40179f0d56
Oskar Wickström <oskar@wickstrom.tech>
2026-01-30 08:14:36 +0100

    upgrade git settings format

--- git.nix
+++ git.nix
@@ -8,8 +8,6 @@
   home.packages = [ pkgs.gh ];
   programs.git = {
     enable = true;
-    userName = "Oskar Wickström";
-    userEmail = "oskar@wickstrom.tech";
     ignores = [
       ".vimrc"
       ".direnv"
@@ -21,31 +19,6 @@
       ".project"
       ".settings"
     ];
-    aliases = {
-      aa = "add --all";
-      br = "branch";
-      co = "checkout";
-      c = "commit -m";
-      ci = "commit";
-      cl = "clean";
-      cp = "commit -p";
-      s = "status";
-      st = "status";
-
-      # `git diff` with difftastic.
-      d = "-c diff.external=difft diff";
-      dc = "-c diff.external=difft diff --cached";
-
-      # `git log` with patches shown with difftastic.
-      dl = "-c diff.external=difft log -p --ext-diff";
-      # Show the most recent commit with difftastic.
-      ds = "-c diff.external=difft show --ext-diff";
-
-      pl = "pull";
-      ps = "push";
-      h = "git log --graph --pretty=oneline --abbrev-commit";
-      l = "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
-    };
     settings = {
       core.editor = "vim";
       credential.helper = "store --file ~/.git-credentials";
@@ -58,6 +31,37 @@
       mergetool.keepBackup = false;
       mergetool.nvimdiff.layout = "LOCAL,MERGED,REMOTE";
       mergetool.nvimdiff.trustExitCode = false;
+
+      user = {
+        name = "Oskar Wickström";
+        email = "oskar@wickstrom.tech";
+      };
+
+      aliases = {
+        aa = "add --all";
+        br = "branch";
+        co = "checkout";
+        c = "commit -m";
+        ci = "commit";
+        cl = "clean";
+        cp = "commit -p";
+        s = "status";
+        st = "status";
+
+        # `git diff` with difftastic.
+        d = "-c diff.external=difft diff";
+        dc = "-c diff.external=difft diff --cached";
+
+        # `git log` with patches shown with difftastic.
+        dl = "-c diff.external=difft log -p --ext-diff";
+        # Show the most recent commit with difftastic.
+        ds = "-c diff.external=difft show --ext-diff";
+
+        pl = "pull";
+        ps = "push";
+        h = "git log --graph --pretty=oneline --abbrev-commit";
+        l = "log --graph --pretty='%Cred%h%Creset - %C(bold blue)<%an>%Creset %s%C(yellow)%d%Creset %Cgreen(%cr)' --abbrev-commit --date=relative";
+      };
     };
   };
 }
