ae7e9328219adf8c96a5b2d9aa396e0fdca52ace
Oskar Wickström <oskar@wickstrom.tech>
2026-05-12 21:46:23 +0200

    longer description in jj zsh prompt

--- zsh.nix
+++ zsh.nix
@@ -42,7 +42,7 @@
       export NIX_PATH=$HOME/.nix-defexpr/channels''${NIX_PATH:+:}$NIX_PATH
 
       _my_theme_vcs_info() {
-        jj_prompt_template_raw ' "[" ++ concat( separate(" ", "%B" ++ format_short_change_id_with_change_offset(self) ++ "%b", bookmarks,if(conflict,label("conflict","conflict")), "%F{green}+" ++ self.diff().stat(0).total_added() ++ "%f/%F{red}-" ++ self.diff().stat(0).total_removed() ++ "%f", coalesce(truncate_end(10, self.description().first_line()), "(no description)")) )++"]" ' \
+        jj_prompt_template_raw ' "[" ++ concat( separate(" ", "%B" ++ format_short_change_id_with_change_offset(self) ++ "%b", bookmarks,if(conflict,label("conflict","conflict")), "%F{green}+" ++ self.diff().stat(0).total_added() ++ "%f/%F{red}-" ++ self.diff().stat(0).total_removed() ++ "%f", coalesce(truncate_end(20, self.description().first_line(), "…"), "(no description)")) )++"]" ' \
         || git_prompt_info
       }
 
