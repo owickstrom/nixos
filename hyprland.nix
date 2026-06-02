31f2eb5fc70b7e519731dce6a8517baad42b7e81
Oskar Wickström <oskar@wickstrom.tech>
2026-06-02 15:20:54 +0200

    monitor and ai update

--- hyprland.nix
+++ hyprland.nix
@@ -30,10 +30,10 @@ in
 
     monitor = [
       "eDP-1,highres,auto-left,1.5,transform,0" # laptop monitor
-      "desc:BNQ BenQ EW2880U ET4AM01231SL0,highres,auto-right,2,transform,1" # ystad lcd monitor
+      # "desc:BNQ BenQ EW2880U ET4AM01231SL0,highres,auto-right,2,transform,1" # black lcd monitor
       "HDMI-A-2,highres,auto-down,2,transform,1"
-      "DP-1,2560x1440@60.00Hz,auto-right,1.6,transform,1" # ystad eink monitor
-      "DP-3,highres,auto-up,2,transform,0" # puget external monitor
+      "DP-1,2560x1440@60.00Hz,auto-right,1.6,transform,1" # eink monitor
+      "HDMI-A-1,highres,auto-right,2,transform,1" # lcd monitor
       ", preferred, auto, 1"
     ];
 
